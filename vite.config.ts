a1a73784316a23bf01992d69d2b2883f0eee047c
Avril-Cui <avrilcui17@gmail.com>
2025-11-16 15:41:22 -0500

    fix vue

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
-export default defineConfig(async ({ mode }) => {
-  const plugins = [vue(), vueJsx()]
-
-  // Only load and enable devtools in development mode
-  if (mode === 'development') {
-    const vueDevTools = (await import('vite-plugin-vue-devtools')).default
-    plugins.push(vueDevTools())
-  }
-
-  return {
-    plugins,
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  }
+  },
 })
